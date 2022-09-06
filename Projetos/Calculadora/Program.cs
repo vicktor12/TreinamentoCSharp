e5a8b12510af168c67a73cc8c68bbb7734c9eec5
vicktor12 <victorsouza0@icloud.com>
2022-09-06 19:44:50 -0300

    4º projeto | Calculador usando switch - case

---
+++ Projetos/Calculadora/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculadora {
+
+    class Program {
+
+        static void Main (string[] args){
+
+            double num1, num2;
+            string operador;
+
+            Console.Write("Informe o 1º numero: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Informe o 2º numero: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Agora informa qual operação deseja realizar ### somar, subtrair, dividir, multiplicar | ou informe operador correspondente, exemplo + ###");
+            operador = Console.ReadLine();
+
+            switch(operador){
+                case "somar":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "subtrair":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "dividir":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                case "multiplicar":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                default:
+                    Console.WriteLine("Operador informado inválido - Aplicação será encerrada");
+                    break;
+            }
+        }
+    }
+}
