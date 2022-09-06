0a98b241046fe3fcc6bd26c80887de15eed6f030
vicktor12 <victorsouza0@icloud.com>
2022-09-06 16:49:02 -0300

    Meu primeiro projeto em C# com console application

---
+++ Projetos/ConversorTemperaturas/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConversorTemperaturas {
+
+    class Program {
+
+        static void Main (string[] args){
+
+            double c, f, k;
+
+            Console.WriteLine("### CONVERSOR DE TEMPERATURAS ####");
+            Console.Write("Insira a Temperatura em Celsius ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------------");
+
+            f = ((c * 9)/5) + 32;
+            k = c + 273.15;
+
+            Console.WriteLine("A temperatura em Celsius informada foi : " + c);
+            Console.WriteLine("Em Fahrenheit fica: " + f);
+            Console.WriteLine("Em Kelvin fica: " + k);
+        }
+    }
+}
