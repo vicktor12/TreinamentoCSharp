a8394d85c88a8eb41e69bb6c517882404d68427c
vicktor12 <victorsouza0@icloud.com>
2022-09-06 20:36:10 -0300

    Projeto 5 | Sequencia de Fibonacci

---
+++ Projetos/SequenciaFibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SequenciaFibonacci {
+
+    class Program {
+
+        static void Main (string[] args){
+
+            int num1 = 0, num2 = 1, c = 0;
+            
+            Console.Write("Deseja que seja impresso quantos numeros? ");
+            int valor = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < valor; i++){
+                if(i < valor -1)
+                    Console.Write(num1 + ", ");
+                else
+                    Console.Write(num1);
+
+                c = num1 + num2;
+                num1 = num2;
+                num2 = c;
+            }
+        }
+    }
+}
