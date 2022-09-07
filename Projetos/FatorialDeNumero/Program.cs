9e8945198f2ff64f370c79b29b582c1bd2d3bff3
vicktor12 <victorsouza0@icloud.com>
2022-09-07 13:56:38 -0300

    Novo Projeto | Fatorial de um Numero

---
+++ Projetos/FatorialDeNumero/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FatorialDeNumero
+{
+    
+    class Program
+    {
+
+        static void Main (string[] args)
+        {
+
+            Console.Write("calcular fatorial de qual numero? ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O fatorial de " + num + " é " + Fatorial(num));
+        }
+
+        static int Fatorial(int num)
+        {
+            if (num <= 0)
+            {
+                return 1;
+            }
+            return num * Fatorial(num -1);
+        }
+    }
+}
