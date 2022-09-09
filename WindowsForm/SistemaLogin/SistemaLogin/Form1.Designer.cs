b6c7e74bd952bac9689e1b3f682f2a78f86f56c2
vicktor12 <victorsouza0@icloud.com>
2022-09-09 12:12:40 -0300

    Novo Projeto | Sistema de Login

---
+++ WindowsForm/SistemaLogin/SistemaLogin/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace SistemaLogin
+{
+    partial class FormHome
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWelcome.Location = new System.Drawing.Point(114, 67);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(497, 116);
+            this.labelWelcome.TabIndex = 0;
+            this.labelWelcome.Text = "Tela Inicial";
+            this.labelWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelWelcome.Click += new System.EventHandler(this.labelWelcome_Click);
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 353);
+            this.Controls.Add(this.labelWelcome);
+            this.Name = "FormHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWelcome;
+    }
+}
+
